7315b903fc1bcb7aceae940ddf15c4f1adacf7bd
Anthony Hung <hungtran@saigontechnology.com>
2019-12-23 09:25:01 +0700

    Link CSS JS files.

--- bootstrap-admin-layout/src/App.vue
+++ bootstrap-admin-layout/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <Navigation />
+    <Sidebar />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,29 +9,18 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Navigation from "./components/layout/navigation.vue";
+import Sidebar from "./components/layout/sidebar.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation,
+    Sidebar
   }
 };
 </script>
 
 <style>
-@import "./assets/plugins/fontawesome-free/css/all.min.css";
-@import "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
-@import "./assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css";
-@import "./assets/dist/css/adminlte.min.css";
-@import "./assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css";
-@import "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700";
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
