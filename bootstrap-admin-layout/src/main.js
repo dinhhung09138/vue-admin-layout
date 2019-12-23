5ee9ac99ae5968a889c53bb5c1a9086f034f7236
Anthony Hung <hungtran@saigontechnology.com>
2019-12-23 16:17:13 +0700

    Adding validation library into project

--- bootstrap-admin-layout/src/main.js
+++ bootstrap-admin-layout/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate';
 import App from './App.vue'
 import router from './router/router';
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate);
 
 new Vue({
     el: '#app',
