999aba5ed7678f1c56aa4e58cceeeff66b60cbc9
Anthony Hung <hungtran@saigontechnology.com>
2019-12-24 08:50:45 +0700

    Add vuetify component

--- bootstrap-admin-layout/src/main.js
+++ bootstrap-admin-layout/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate';
+import Vuetify from 'vuetify';
 import App from './App.vue'
 import router from './router/router';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate);
+Vue.use(Vuetify);
 
 new Vue({
     el: '#app',
