7775cbf241ec5bb50d4fa30d2088d805be78c186
Anthony Hung <hungtran@saigontechnology.com>
2019-12-24 16:13:19 +0700

    Add icon font

--- bootstrap-admin-layout/src/main.js
+++ bootstrap-admin-layout/src/main.js
@@ -1,6 +1,11 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css';
+
+import Vuetify from 'vuetify';
+
 import Vue from 'vue'
 import Vuelidate from 'vuelidate';
-import Vuetify from 'vuetify';
 import App from './App.vue'
 import router from './router/router';
 
