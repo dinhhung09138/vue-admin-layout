7b235b6dabd609daefc3c442574807fcf48c50d4
Anthony Hung <hungtran@saigontechnology.com>
2019-12-25 11:17:37 +0700

    Add navigation and bottom bar

--- bootstrap-admin-layout/src/layouts/layout.vue
+++ bootstrap-admin-layout/src/layouts/layout.vue
@@ -1,21 +1,25 @@
 <template>
   <v-app>
-    <v-card flat tile>
-      <v-toolbar color="primary" dark>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Title</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </v-card>
+    <v-app-bar color="blue darken-3" dark dense tile flat fixed>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-badge>
+          <template v-slot:badge>0</template>
+          <v-icon>mdi-email</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
     <v-row>
       <v-col cols="12" md="12"></v-col>
     </v-row>
@@ -27,6 +31,13 @@
       </div>
       <Footer />
     </div>
+    <v-bottom-navigation background-color="blue darken-3" color="white" dense tile flat>
+      <v-btn tile outlined x-small color="success">
+        <span>Favorites</span>
+      </v-btn>
+      <span>Data content</span>
+      abc def
+    </v-bottom-navigation>
   </v-app>
 </template>
 <script>
